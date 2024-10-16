f3aaf864eb17c2806fd45a9d5ab43883b497dcd0
Robert Schulz <schulzr@gmail.com>
2024-10-16 12:55:53 +0200

    0.0.22 - BugFix metrics.plot_confusion_matrix: close plot

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.21' 
+VERSION = '0.0.22' 
 DESCRIPTION = 'Machine Learning'
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
