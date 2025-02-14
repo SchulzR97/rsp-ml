cbb93119697d04b512c9b757777ebd4bcaf02a42
Robert Schulz <schulzr@gmail.com>
2025-02-14 15:07:54 +0100

    0.0.88 - handle corrupted archives

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages, find_namespace_packages
 
-VERSION = '0.0.87' 
+VERSION = '0.0.88' 
 DESCRIPTION = 'Machine Learning'
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
